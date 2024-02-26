be6e5e36254cbea641904f0cb9fb4bb948396615
CutyDina <103970174+cutydina@users.noreply.github.com>
2024-02-25 20:33:32 -0600

    Update TouchMove LeftRight.cs

--- TouchMove LeftRight.cs
+++ TouchMove LeftRight.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CaterpillarMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Touch touch;
     private float moveSpeed;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
